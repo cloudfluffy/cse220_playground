c7a18437e23f786e7f62bc0e84ec0239cee9f6bc
Bote Zhou <120070429+cloudfluffy@users.noreply.github.com>
2025-02-18 02:22:07 +0000

    delete empty lines

--- pointer16.c
+++ pointer16.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main() {
     int x = 0x455343;
     // x is stored in little endian
@@ -10,17 +9,13 @@ int main() {
     // 0x53 = 'S'
     // 0x45 = 'E'
     // 0x00 = '\0'
-
     char* x_char_ptr = &x;
-
     printf("0x%0x\n", x);
     printf("%c = 0x%02x\n", *x_char_ptr, *x_char_ptr);
     printf("%c = 0x%02x\n", *(x_char_ptr + 1), *(x_char_ptr + 1));
     printf("%c = 0x%02x\n", *(x_char_ptr + 2), *(x_char_ptr + 2));
     printf("%c = 0x%02x\n", *(x_char_ptr + 3), *(x_char_ptr + 3));
-
     printf("\n");
-
     x_char_ptr[2] = 'S';
     // +------+------+------+------+
     // | 0x43 | 0x53 | 0x53 | 0x00 |
@@ -29,13 +24,11 @@ int main() {
     // 0x53 = 'S'
     // 0x45 = 'S'
     // 0x00 = '\0'
-
     printf("0x%0x\n", x);
     printf("%c = 0x%02x\n", *x_char_ptr, *x_char_ptr);
     printf("%c = 0x%02x\n", *(x_char_ptr + 1), *(x_char_ptr + 1));
     printf("%c = 0x%02x\n", *(x_char_ptr + 2), *(x_char_ptr + 2));
     printf("%c = 0x%02x\n", *(x_char_ptr + 3), *(x_char_ptr + 3));
-
     // A pointer is just a variable that holds a memory address.
     // We are responsible for keeping track of what lives at that address.
 }
